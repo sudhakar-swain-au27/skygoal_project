f6d4571bd4c38cfee303e7dbe193ef615414f365
Sudhakar Swain <91896763+sudhakar-swain-au27@users.noreply.github.com>
2023-02-10 08:49:28 +0530

    Skygoal frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Signup from "./pages/SignUp";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Cards from "./pages/Cards";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/register" element={<Signup />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/" element={<Cards />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
